857a7a78cb1194b00f491a2ba71d16b17567764c
Lino Conord <158364911+LinoVation1312@users.noreply.github.com>
2024-12-19 11:24:16 +0100

    Update datacomparaison.py

--- datacomparaison.py
+++ datacomparaison.py
@@ -105,7 +105,7 @@ if all_series:
     )
 
     # Function to smooth the data with logarithmic interpolation
-    def smooth_curve(frequencies, values, num_points=300):
+    def smooth_curve(frequencies, values, num_points=150):
         """
         Smooth the curve using spline interpolation on a logarithmic scale.
         
